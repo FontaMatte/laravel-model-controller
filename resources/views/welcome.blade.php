7f5a0d5a22dc39a12118cef66126a6e182fe7d15
FontaMatte <matteo.fontanel@gmail.com>
2023-03-10 15:33:35 +0100

    Excersise completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 + Bootstrap Template</title>
+        <title>Laravel Model Controller</title>
 
         {{-- Includiamo gli assets con la direttiva @vite --}}
         @vite('resources/js/app.js')
@@ -12,19 +12,35 @@
     <body>
 
         <main>
-            <div class="container">
+            <div class="container mt-5">
                 <div class="row">
-                    <div class="col text-center">
-
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
+                    @foreach ($movies as $movie)
+                        <div class="col-3">
+                            <div class="card mb-4">
+                                <ul>
+                                    <li>
+                                        ID: {{ $movie->id }}
+                                    </li>
+                                    <li>
+                                        TITLE: {{ $movie->title }}
+                                    </li>
+                                    <li>
+                                        ORIGINAL TITLE: {{ $movie->original_title }}
+                                    </li>
+                                    <li>
+                                        NATIONALITY: {{ $movie->nationality }}
+                                    </li>
+                                    <li>
+                                        RELEASE DATE: {{ $movie->date }}
+                                    </li>
+                                    <li>
+                                        VOTE: {{ $movie->vote }}
+                                    </li>
+                                </ul>
                             </div>
-                        </div>
 
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </main>
